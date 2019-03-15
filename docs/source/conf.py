bb14fe90de25b79b4aaa09300b521bf1270c9636
nrweir <nicholas.r.weir@gmail.com>
2019-03-15 16:05:25 -0400

    fixing api doc structure

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,7 +45,8 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages'
+    'sphinx.ext.githubpages',
+    'sphinx.ext.autosummary'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
